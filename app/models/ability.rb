5ee7963d33f8d1b4254284ddfa3fb5002a90ae6f
Angela2389 <angela_2389@hotmail.com>
2016-08-23 18:03:06 +0200

    authorization added

---
+++ app/models/ability.rb
@@ -0,0 +1,19 @@
+class Ability
+  include CanCan::Ability
+
+  def initialize(user)
+    user ||= User.new # guest user (not logged in)
+
+    # Can READ anything
+    can :read, :all
+
+    if user.persisted? # in db, so logged in
+      # Can MANAGE (create, read, update, destroy, etc.) own Post
+      can :manage, Project, user: user
+      can :manage, Phase, user: user
+      can :manage, Team, user: user
+      can :manage, Spring, user: user
+      can :manage, Experiment, user: user
+    end
+  end
+end
