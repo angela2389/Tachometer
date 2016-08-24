3b634e7a49cd2b8ae25e9ddf209b7b4ec49a0598
caisepolente <steve.durighello@gmail.com>
2016-08-24 12:58:22 +0200

    show experiment view working with nested route

--- app/controllers/phases_controller.rb
+++ app/controllers/phases_controller.rb
@@ -69,8 +69,8 @@ class PhasesController < ApplicationController
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_phase
-      project = Project.find(params[:project_id])
-      @phase = project.phases.find(params[:id])
+      @project = Project.find(params[:project_id])
+      @phase = @project.phases.find(params[:id])
     end
 
     # Never trust parameters from the scary internet, only allow the white list through.
--- config/routes.rb
+++ config/routes.rb
@@ -1,20 +1,16 @@
 Rails.application.routes.draw do
   devise_for :users
-  resources :experiments
+ 
   get 'projects/:id/phases/new' => 'phases#new', as: :new_phase_in_project
-  get 'projects/:project_id/phases/:phase_id/sprints/new' => 'sprints#new', as: :new_sprint_in_phase
-  get 'projects/:project_id/phases/:phase_id/sprints/' => 'sprints#index', as: :sprints
-  get 'projects/:project_id/phases/:phase_id/sprints/:id/edit' => 'sprints#edit', as: :edit_sprint
-  get 'projects/:project_id/phases/:phase_id/sprints/:id' => 'sprints#show', as: :sprint
-
 
   resources :projects do
     resources :phases do
-      resources :sprints
+      resources :experiments do
+	    resources :sprints
+	  end
     end
   end
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-
+ 
   root 'projects#index'
 
   post 'send_weekly_emails' => 'projects#send_weekly_emails', as: :email_sent
