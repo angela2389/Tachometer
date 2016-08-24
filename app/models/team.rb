6d90dd15d76e75eadba540aad579f451eca879d0
caisepolente <steve.durighello@gmail.com>
2016-08-24 12:07:52 +0200

    added has_many :experiments in team

--- app/models/team.rb
+++ app/models/team.rb
@@ -2,6 +2,7 @@ class Team < ApplicationRecord
   belongs_to :phase
   has_many :users, through: :team_members
   has_many :team_members
+  has_many :experiments
 
   ROLE = ["Product Owner", "Advisor", "Team Member", "Coach"]
 end
