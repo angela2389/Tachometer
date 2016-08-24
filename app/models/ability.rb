f7fb8ec97802a0c7304762bd80dedfdcbb0a6ce4
Angela2389 <angela_2389@hotmail.com>
2016-08-24 19:51:38 +0200

    changed the manage:all back to guest

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -3,14 +3,12 @@ class Ability
 
   def initialize(user)
     user ||= User.new # guest user (not logged in)
-
     # Can READ anything
     can :manage, :all
 
     if user.persisted? # in db, so logged in
       # Can MANAGE (create, read, update, destroy, etc.) own Post
-      can :manage, :all
-      # can :manage, Project, user: user
+      can :manage, :all if TeamMember.find_by(user_id: user.id).role == "Product Owner"
       # can :manage, Phase, user: user
       # can :manage, Team, user: user
       # can :manage, Spring, user: user
