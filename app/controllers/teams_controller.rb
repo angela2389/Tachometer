80bc4ee07bb3bd95089fb0f4191a1c3b5f038797
ckreeftmeijer <ckreeftmeijer@gmail.com>
2016-08-25 15:27:11 +0200

    fixed calculation for progress

--- app/controllers/teams_controller.rb
+++ app/controllers/teams_controller.rb
@@ -6,8 +6,7 @@ class TeamsController < ApplicationController
     # GET /phases/1
     # GET /phases/1.json
     def show
-      project = Project.find(params[:project_id])
-      @team = project.teams.find(params[:id])
+      @team = Team.find(params[:id])
       @team_member = @team.team_members.new
     end
 
