b7b990300ac73362ea3bf019d1d41cbf61b91d02
Mark Nuiver <mark@vrijblikken.nl>
2016-08-25 12:35:17 +0200

    Add sprint finalize route and sprint#action in the controller

--- app/controllers/sprints_controller.rb
+++ app/controllers/sprints_controller.rb
@@ -63,6 +63,9 @@ class SprintsController < ApplicationController
     end
   end
 
+  def finalize
+  end
+
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_project
