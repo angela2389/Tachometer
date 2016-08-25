4d81ab4466ffa541976ba510a466a025e66a5a8b
Angela2389 <angela_2389@hotmail.com>
2016-08-25 15:54:05 +0200

    enable manage all by guest

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -4,13 +4,13 @@ class Ability
   def initialize(user)
     user ||= User.new # guest user (not logged in)
     # Can READ anything
-    can :read, :all
+    can :manage, :all
 
     if user.persisted? # in db, so logged in
       if user.role == "admin"
       can :manage, :all
       can :access, :rails_admin
-      can :dashboard 
+      can :dashboard
       # Can MANAGE (create, read, update, destroy, etc.) own Post
       else
       can :create, :all
